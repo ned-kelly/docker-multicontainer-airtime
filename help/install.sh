131941bb2a643955a37dbb739db7b12d64e288e3
Víctor Rojas <okvic77@me.com>
2015-07-26 14:21:50 -0500

    Please!!!!

--- help/install.sh
+++ help/install.sh
@@ -6,7 +6,7 @@ sudo service rabbitmq-server start
 
 mkdir ~/airtime
 curl -L https://github.com/sourcefabric/airtime/archive/2.5.x.tar.gz | tar --strip-components=1 -C ~/airtime -xz
-sudo ~/airtime/install -fa
+sudo ~/airtime/install -fapi
 
 sudo cp ~/helpers/htaccess /usr/share/airtime/public/.htaccess
 sudo php5enmod opcache
