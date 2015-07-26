eb944bd5129abf4ca96997eec16e8f5e74a4e20c
Víctor Rojas <okvic77@me.com>
2015-07-26 12:41:19 -0500

    Docker stanlone.

--- help/apache2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-php /help/config.php
-#service airtime-media-monitor start && service airtime-playout restart && service airtime-liquidsoap restart && tail -f /var/log/airtime/media-monitor/media-monitor.log
-source /etc/apache2/envvars
-exec apache2 -D FOREGROUND
