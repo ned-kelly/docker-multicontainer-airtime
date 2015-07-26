d86704b7660da2fa546c40091a1f781a7f8bfb0d
Víctor Rojas <okvic77@me.com>
2015-07-26 16:59:33 -0500

    Try

--- help/install.sh
+++ help/install.sh
@@ -12,6 +12,11 @@ sudo ~/airtime/install -fapi
 sudo mkdir -p /srv/airtime/stor
 sudo chown -R airtime:www-data /srv/airtime/stor/
 
+
 sudo cp ~/helpers/htaccess /usr/share/airtime/public/.htaccess
+sudo cp ~/helpers/config /etc/airtime/airtime.conf
+sudo cp ~/helpers/media /etc/airtime/media_monitor_logging.cfg
+
+
 sudo php5enmod opcache
 sudo rm -rf ~/airtime
