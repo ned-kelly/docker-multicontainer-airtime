681feedc7953e1fd6da1c99cd0326021c42c4da7
Víctor Rojas <okvic77@me.com>
2015-07-26 15:20:23 -0500

    Permissions

--- help/install.sh
+++ help/install.sh
@@ -8,7 +8,7 @@ mkdir ~/airtime
 curl -L https://github.com/sourcefabric/airtime/archive/2.5.x.tar.gz | tar --strip-components=1 -C ~/airtime -xz
 sudo ~/airtime/install -fapi
 
-sudo chown -R airtime:www-data .
+sudo chown -R airtime:www-data /srv/airtime/stor/
 
 sudo cp ~/helpers/htaccess /usr/share/airtime/public/.htaccess
 sudo php5enmod opcache
