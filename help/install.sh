0d25ed33027d8b1bcfc786ce37593af46b30e4f1
Víctor Rojas <okvic77@me.com>
2015-07-26 14:46:28 -0500

    Please last

--- help/install.sh
+++ help/install.sh
@@ -8,6 +8,8 @@ mkdir ~/airtime
 curl -L https://github.com/sourcefabric/airtime/archive/2.5.x.tar.gz | tar --strip-components=1 -C ~/airtime -xz
 sudo ~/airtime/install -fapi
 
+sudo chown -R airtime:www-data .
+
 sudo cp ~/helpers/htaccess /usr/share/airtime/public/.htaccess
 sudo php5enmod opcache
 sudo rm -rf ~/airtime
