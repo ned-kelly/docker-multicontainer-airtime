fe62212231a3e2299a20912e7f8b861a4223e7b2
Víctor Rojas <okvic77@me.com>
2015-07-26 13:17:15 -0500

    NOPASSWD

--- help/pre.sh
+++ help/pre.sh
@@ -1,6 +1,6 @@
 echo "127.0.0.1 airtime" >> /etc/hosts
 locale-gen --purge en_US.UTF-8 && echo -e 'LANG="$LANG"\nLANGUAGE="$LANGUAGE"\n' > /etc/default/locale
-echo "airtime   ALL=(ALL:ALL) ALL" >> /etc/sudoers
+echo "airtime   ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 adduser --disabled-password --gecos "" airtime
 apt-get update
 apt-get install -y rabbitmq-server apache2 curl supervisor
